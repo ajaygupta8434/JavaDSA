3012f1ec4d2478788f693ffb07b4d67ab44576bf
Ajay Gupta <148214882+ajaygupta8434@users.noreply.github.com>
2026-02-04 09:50:12 +0530

    my

---
+++ stack.java
@@ -0,0 +1,42 @@
+class Innerstack {
+int size;
+int top;
+int stack[];
+Innerstack(int size){
+    this.size=size;
+    this.top=-1;
+    this.stack=new int[size];
+}
+//push data into stack
+public void push(int val){
+    if(top==size-1){
+        System.out.println("Stack Overflow");
+        return;
+    }
+    top++;
+    stack[top]=val;
+}
+//pop data from stack
+public int pop(){
+    if(top==-1){
+        System.out.println("Stack Underflow");
+        return -1;
+    }
+    int val=stack[top];
+    top--;
+    return val;
+}
+}
+public class stack {
+    public static void main(String[] args) {
+        Innerstack s=new Innerstack(5);
+        s.push(10);
+        s.push(20);
+        s.push(30);
+        System.out.println(s.pop());
+        System.out.println(s.pop());
+        System.out.println(s.pop());
+        System.out.println(s.pop());
+    }
+}
+
