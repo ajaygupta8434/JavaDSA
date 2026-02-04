2b1d701302856cf7ad263f7076f86cadd305cdf6
Ajay Gupta <148214882+ajaygupta8434@users.noreply.github.com>
2026-02-04 09:51:39 +0530

    my

--- stack.java
+++ stack.java
@@ -31,7 +31,7 @@ public class stack {
     public static void main(String[] args) {
         Innerstack s=new Innerstack(5);
         s.push(10);
-        s.push(20);
+        s.push(15);
         s.push(30);
         System.out.println(s.pop());
         System.out.println(s.pop());
